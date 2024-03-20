c33d07b78901843c7f35b47de46fbfccd78e40ef
ev-indrik <delfinmur@gmail.com>
2024-03-20 13:34:42 +0200

    css acctive02 problem solved, popUp styles corrected

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ cities.forEach(function (city) {
     this.classList.add("active");
   });
 });
+
+const loginIcon = document.querySelector(".login_icon");
+const popUp = document.querySelector(".pop_up");
+
+let isLogin = false;
+
+loginIcon.addEventListener("click", () => {
+  popUp.classList.add("active02");
+});
+
+// loginIcon.addEventListener("click", () => {
+//   popUp.classList.add("active02");
+//   if (!isLogin) {
+//     popUp.classList.add("active03");
+//     isLogin = true;
+//   } else {
+//     popUp.classList.add("active04");
+//     isLogin = false;
+//   }
+// });
