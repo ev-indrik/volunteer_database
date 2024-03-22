90198300356435ad2cf7ff1341a228938e181368
ev-indrik <delfinmur@gmail.com>
2024-03-22 22:26:35 +0200

    log in and log out fuctionality

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 let isLogin = false;
+const loginIcon = document.querySelector(".login_icon");
+const popUp = document.querySelector(".pop_up");
+const popUpText = popUp.querySelector("p");
+const loginIconImg = document.getElementById("login_icon_img");
+
 localStorage.setItem("isLogin", isLogin);
 
-const cities = document.querySelectorAll(".city");
+function logIn() {
+  isLogin = true;
+  loginIcon.classList.add("logged_in_user");
+  loginIconImg.src = "./resources/user_logged.jpg";
+}
 
+function logOut() {
+  isLogin = false;
+  loginIcon.classList.remove("logged_in_user");
+  loginIconImg.src = "./resources/User_02.svg";
+}
+
+const cities = document.querySelectorAll(".city");
 cities.forEach(function (city) {
   city.addEventListener("click", function () {
     cities.forEach(function (city) {
@@ -12,11 +28,22 @@ cities.forEach(function (city) {
   });
 });
 
-const loginIcon = document.querySelector(".login_icon");
-const popUp = document.querySelector(".pop_up");
-
-loginIcon.addEventListener("click", () => {
+loginIcon.addEventListener("click", (e) => {
+  e.stopPropagation();
   popUp.classList.toggle("active02");
+  if (!isLogin) {
+    popUpText.innerText = "Log in";
+  } else {
+    popUpText.innerText = "Log out";
+  }
+});
+
+popUp.addEventListener("click", () => {
+  if (!isLogin) {
+    logIn();
+  } else {
+    logOut();
+  }
 });
 
 // popUp.addEventListener("click", () => {
